3c6a594e679ad70af5b02c1e4017c7af5c9ec311
ghost <ghostpy91.com>
2025-03-21 03:44:43 -0700

    completed

--- do-bro-server/index.js
+++ do-bro-server/index.js
@@ -8,7 +8,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"], 
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://do-bro.web.app",
+  ],
   credentials: true,
   optionsSuccessStatus: 200,
 };
